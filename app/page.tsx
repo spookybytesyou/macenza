1020b4ed613cdd6eb2e2e609c927874718baeb08
spooky <spookyeats4skin@gmail.com>
2026-05-14 23:46:58 +0530

    add sementical comment for about section

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,15 @@
 "use client";
 
 import Hero from "@/components/Hero";
-import Services from "@/components/Services";
 import TestimonialsWithCarousel from "@/components/testimonials-with-carousel";
+import Services from "@/components/Services";
 
 export default function Home() {
   return (
     <div className="main-wrapper">
 
       <Hero />
+      {/* About Section resides under Hero section */}
       <TestimonialsWithCarousel />
       <Services />
 
